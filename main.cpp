367799139aff5602496095cfb2c78e7d56fd006e
NikeNoke <olszowkathomas@gmail.com>
2023-03-16 17:19:32 +0100

    version 0.2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include "DesignByContract.h"
 #include "TinyXML/tinyxml.h"
-#include "Tram.h"
-#include "Station.h"
+#include "Tram/Tram.h"
+#include "Station/Station.h"
+#include "vector"
+#include "./MetroNet/MetroNet.h"
+#include "Utils/utils.h"
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    TiXmlDocument doc;
+    if (!doc.LoadFile("metronet.xml")) {
+        std::cerr << doc.ErrorDesc() << std::endl;
+        return 1;
+    }
+    TiXmlElement *root = doc.FirstChildElement();
+    if (root == NULL) {
+        std::cerr << "Failed to load file: No root element." << std::endl;
+        doc.Clear();
+        return 1;
+    }
+    MetroNet metroNet;
+    for (TiXmlElement *element = root->FirstChildElement(); element != NULL; element = element->NextSiblingElement()) {
+
+        std::string current = element->Value();
+        if (current == "STATION") {
+
+            Utils::parseStation(metroNet, element);
+
+        } else if (current == "TRAM") {
+
+            Utils::parseTram(metroNet, element);
+
+        } else{
+            std::cerr << "Deze element is ongekend!\n";
+        }
+    }
+    //TODO maybe verification of viability of metroNet???
+    doc.Clear();
+
+
+    //2.2
+    std::ofstream metroNetSpecs;
+
+    metroNetSpecs.open("metroNetSpecs.txt");
+
+    Utils::writeSpecs(metroNetSpecs, metroNet);
+
+    metroNetSpecs.close();
+
 }
